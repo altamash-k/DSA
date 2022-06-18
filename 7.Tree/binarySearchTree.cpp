b8d6455bf0f0ba547976b775855ee94d4ff7d3d1
Altmash <s.altamash.r@gmail.com>
2022-06-18 22:28:01 +0530

    bst

--- 7.Tree/binarySearchTree.cpp
+++ 7.Tree/binarySearchTree.cpp
@@ -29,6 +29,54 @@ node *insertBST(node *root, int val)
     return root;
 }
 
+node *search(struct node *root, int key)
+{
+    if(root == NULL)
+        return NULL;
+    if(key == root->data)
+        return root;
+    else if(key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+node *inorderSucc(struct node *root)
+{
+    node *curr = root;
+    while(curr->left != NULL)
+        curr = curr->left;
+    
+    return curr;
+}
+
+node *deleteInBST(struct node *root, int key)
+{
+    if(key < root->data)
+        root->left = deleteInBST(root->left, key);
+    else if(key > root->data)
+        root->right = deleteInBST(root->right, key);
+    else
+    {
+        if(root->left == NULL)
+        {
+            node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if(root->right == NULL)
+        {
+            node *temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        node *temp = inorderSucc(root->right);
+        root->data = temp->data;
+        root->right = deleteInBST(root->right, temp->data);
+    }
+}
+
 void inorder(struct node *root)
 {
     if(root == NULL)
@@ -48,7 +96,26 @@ int main()
     insertBST(root, 2);
     insertBST(root, 7);
 
+    /*     5
+          /  \
+         1    7
+          \
+           3
+          /  \
+         2    4
+    */
+
     cout << "Inorder: ";
     inorder(root);
+
+    if(search(root, 5) == NULL)
+        cout << "\nKey does not exist";
+    else
+        cout << "\nKey exist";
+
+    cout << endl;
+    root = deleteInBST(root, 3);
+    inorder(root);
+
     return 0;
 }

617e85858aa7a81efff7df5f9f29e2d5028aa731
Altmash <s.altamash.r@gmail.com>
2022-06-19 19:47:46 +0530

    arr

---
+++ 3.Arrays/containsDuplicates.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter no of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr+n);
+    cout << "Array elements sorted: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    int duplicates = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == arr[i+1])
+            duplicates = 1;
+    }
+    if(duplicates == 1)
+        cout << "\nContains duplicates";
+    else
+        cout << "\nNo duplicates";
+    
+}

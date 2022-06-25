1b92aa8269e0f9f8e2c5986ccf473c0262b79ca5
Altmash <s.altamash.r@gmail.com>
2022-06-25 19:23:10 +0530

    reverse k nodes

--- 6.Linked List/reverseSLL.cpp
+++ 6.Linked List/reverseSLL.cpp
@@ -43,6 +43,50 @@ void reverse()
     head = prev;
 }
 
+int length()
+{
+	struct node *temp = head;
+	int length = 0;
+	while(temp != NULL)
+	{
+		++length;
+		temp = temp->next;
+	}
+
+	return length;
+}
+
+void reverseKNodes()
+{
+	struct node *prev, *curr, *next;
+	struct node *dummyNode = (struct node*)malloc(sizeof(struct node));
+	dummyNode->data = 0;
+	dummyNode->next = head;
+	prev = dummyNode;
+
+	int len = length();
+	int k;
+	cout << "Enter value of k: ";
+	cin >> k;
+
+	while(len >= k)
+	{
+		curr = prev->next;
+		next = curr->next;
+		for(int i = 1; i < k; i++)
+		{
+			curr->next = next->next;
+			next->next = prev->next;
+			prev->next = next;
+			next = curr->next;
+		}
+		prev = curr;
+		len -= k;
+	}
+	
+	head = dummyNode->next;
+}
+
 void print_list()
 {
 	struct node* p;
@@ -67,7 +111,7 @@ int main()
 	while(1)
 	{
 		
-	cout<<"\nChoose the operations:\n1: Insert a Node\n2: Print the List\n3: Reverse the List"<<endl;
+	cout<<"\nChoose the operations:\n1: Insert a Node\n2: Print the List\n3: Reverse the List\n4: Reverse K nodes\n"<<endl;
 	cin>>ch;
 	switch(ch){
 		case 1:
@@ -78,6 +122,9 @@ int main()
 			break;
 		case 3:
 			reverse();
+			break;
+		case 4:
+			reverseKNodes();
 			break;
 		
 	}		

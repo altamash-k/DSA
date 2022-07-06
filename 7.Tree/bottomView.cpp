319054c55591ddccb8ca537a924c7a3c4a78a64f
Altmash <s.altamash.r@gmail.com>
2022-07-06 21:07:20 +0530

    bottom view of bt

---
+++ 7.Tree/bottomView.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+
+    node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+vector<int> topView(struct node *root)
+{
+    vector<int> ans;
+    if(root == NULL)
+        return ans;
+    
+    map<int, int> mp;
+    queue<pair<node *, int>> q;
+    q.push({root, 0});
+    while(!q.empty())
+    {
+        auto it = q.front();
+        q.pop();
+        node* curr = it.first;
+        int line = it.second;
+
+        mp[line] = curr->data;
+        
+        if(curr->left != NULL)
+            q.push({curr->left, line-1});
+        if(curr->right != NULL)
+            q.push({curr->right, line+1});
+    }
+
+    for(auto it:mp)
+        ans.push_back(it.second);
+    
+    return ans;
+}
+
+int main()
+{
+    struct node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    cout << "Top View: ";
+    vector<int> res = topView(root);
+    for(int i = 0; i < res.size(); i++)
+        cout << res[i] << " ";
+
+
+    return 0;
+}

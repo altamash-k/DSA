9c0c376deb8cb08ad9be90d040a315a9fa50d880
Altmash <s.altamash.r@gmail.com>
2022-07-13 20:33:52 +0530

    eval rpn

---
+++ 4.Stack/evalRPN.cpp
@@ -0,0 +1,57 @@
+/*
+    Evaluate RPN, valid operators: +, -, *, /
+
+    Input: tokens = ["2","1","+","3","*"]
+    Output: 9
+    Explanation: ((2 + 1) * 3) = 9
+
+    Stack, if num push, if operator apply to top 2 nums
+
+    Time: O(n)
+    Space: O(n)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int eval(vector<string>& tokens)
+{
+    stack<int> st;
+    for(int i = 0; i < tokens.size(); i++)
+    {
+        string token = tokens[i]; // get each token
+        if(token.size() > 1 || isdigit(token[0])) // if num
+        {
+            st.push(stoi(token)); // push digit value
+            continue; // continue from loop
+        }
+
+        int num1 = st.top(); // take first nun
+        st.pop();
+        int num2 = st.top(); // take second num
+        st.pop();
+
+        int res = 0;
+
+        // apply required operation
+        if(token == "+")
+            res = num1 + num2;
+        else if(token == "-")
+            res = num1-num2;
+        else if(token == "*")
+            res = num1*num2;
+        else
+            res = num1/num2;
+        
+        st.push(res);
+    }
+
+    return st.top();
+}
+
+int main()
+{
+    vector<string> tokens = { "2","1","+","3","*"};
+ 
+    int res = eval(tokens);
+    cout << res << endl;
+}

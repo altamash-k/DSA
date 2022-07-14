5742c086843bd7e7fa2403a2b910061e0087ce8d
Altmash <s.altamash.r@gmail.com>
2022-07-14 19:23:37 +0530

    changes

--- 1.Basic/invDigits.cpp
+++ 1.Basic/invDigits.cpp
@@ -1,24 +1,16 @@
-#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int n, op, od, id, ip, inv = 0;
+    int n, inv = 0;
     cout << "Enter the number: ";
     cin >> n;
 
-    op=1;
-    while(n!=0)
+    while(n>0)
     {
-        od = n%10;
-        id = op;
-        ip = od;
-        inv = inv + id * pow(10, ip-1);
-
+        inv = inv * 10 + n % 10;
         n = n/10;
-        op++;
-
     }
     cout << inv;
 }
